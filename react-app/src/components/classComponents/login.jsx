e6081428f119847981c9ab870e0c6848c90689b4
RathiPM <rathipriyap.21ece@kongu.edu>
2024-01-25 09:23:08 +0530

    rathi

---
+++ react-app/src/components/classComponents/login.jsx
@@ -0,0 +1,14 @@
+import React from "react"; //JSx is converted to react
+
+class Login extends React.Component {
+    render() { //render-kind of object
+        return (
+            <div>
+                <h1>Class Component Example</h1>
+            </div> //we can empty angle bracket also,but not preferred.
+        );
+
+    }
+}
+
+export default Login;
---
+++ react-app/src/components/functionalComponents/testComponents.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function TestComponent(){
+    return(
+        <div>
+            <h1>This is a test Functional Component.</h1>
+        </div>
+    );
+}
+export default TestComponent;
+/*
+function add(a,b){
+    return a+b
+}
+console.log(add(5,10))
+*/ 
